31ea8adbefd4b9930e71d25c5fe5f403357af33d
Lupin <student.148242@worcesterschools.net>
2024-10-03 10:11:45 -0400

    g

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,10 +1,16 @@
 extends CharacterBody2D
 
+@onready var animated_sprite = $AnimatedSprite2D
 
 const speed = 300.0
 const jump_power = -350.0
 
-var gravity = 900
+var gravity = 900.0
+
+var weapon_equip: bool 
+
+func _ready():
+	weapon_equip = true
 
 
 func _physics_process(delta: float) -> void:
@@ -21,3 +27,29 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, speed)
 
 	move_and_slide()
+	handle_movement_animation(direction)
+
+func handle_movement_animation(dir):
+	if !weapon_equip:
+		if is_on_floor():
+			if !velocity:
+				animated_sprite.play("idle")
+			if velocity:
+				animated_sprite.play("run")
+				toogle_flip_sprite(dir)
+		elif !is_on_floor():
+			animated_sprite.play("fall")
+	if weapon_equip:
+		if is_on_floor():
+			if !velocity:
+				animated_sprite.play("WeaponIdle")
+			if velocity:
+				animated_sprite.play("weapon_run")
+				toogle_flip_sprite(dir)
+		elif !is_on_floor():
+			animated_sprite.play("weapon_fall")
+func toogle_flip_sprite(dir):
+	if dir == 1:
+		animated_sprite.flip_h = false
+	if dir == -1:
+		animated_sprite.flip_h = true
