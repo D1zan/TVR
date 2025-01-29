bb984ae34388d5550fd9108fbb424fbaeec6ddb8
Lupin <student.148242@worcesterschools.net>
2025-01-29 13:01:39 -0500

    fixed stage cameras

--- Scripts/stage_1.gd
+++ Scripts/stage_1.gd
@@ -1,12 +1,16 @@
 extends Node2D
 
 @onready var player_camera = $Player/Camera2D
+@onready var player_camera2 = $Player/stage2_cam
+
+func _on_something_happened(node_to_destroy):
+	node_to_destroy.queue_free()
 
 func _ready():
 	
 	player_camera.enabled = true
 	Global.playerWeaponEquipped = true
-
+	_on_something_happened(player_camera2)
 func _process(delta):
 	if !Global.playerAlive:
 		Global.gameStarted = false
