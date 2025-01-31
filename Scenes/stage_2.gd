983af6bede9cfecb330c588372d425db3e2a816f
LupineReX <student.148242@worcesterschools.net>
2025-01-31 11:38:52 -0500

    added joystick controlled menu

--- Scenes/stage_2.gd
+++ Scenes/stage_2.gd
@@ -15,3 +15,7 @@ func _process(delta):
 	if !Global.playerAlive:
 		Global.gameStarted = false
 		get_tree().change_scene_to_file("res://Scenes/lobby.tscn")
+	var enemies = get_tree().get_nodes_in_group("enemies").size()
+	if enemies == 0:
+		print("clear")
+		get_tree().change_scene_to_file("res://Scenes/lobby.tscn")
