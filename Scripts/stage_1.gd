54715360b6f21db6c7c9986d4d7b892deffed59c
Lupin <student.148242@worcesterschools.net>
2025-02-10 09:38:13 -0500

    removed test prints

--- Scripts/stage_1.gd
+++ Scripts/stage_1.gd
@@ -22,7 +22,6 @@ func _process(delta):
 		
 	var enemies = get_tree().get_nodes_in_group("enemies").size()
 	if enemies == 0:
-		print("clear")
 		get_tree().change_scene_to_file("res://Scenes/stage_2.tscn")
 
 
