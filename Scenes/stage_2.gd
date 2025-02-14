4eb8e2a636e7f7ff06dc6088b3c3a1db760d902a
Lupin <student.148242@worcesterschools.net>
2025-02-14 13:09:29 -0500

    fixed stage 3 not loading

--- Scenes/stage_2.gd
+++ Scenes/stage_2.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 @onready var player_camera = $Player/Camera2D
 @onready var player_camera2 = $Player/stage2_cam
+@onready var player_camera3 = $Player/stage3_cam
 
 func _on_something_happened(node_to_destroy):
 	node_to_destroy.queue_free()
@@ -11,10 +12,12 @@ func _ready():
 	player_camera.enabled = true
 	Global.playerWeaponEquipped = true
 	_on_something_happened(player_camera)
+	_on_something_happened(player_camera3)
+
 func _process(delta):
 	var clear = get_tree().get_nodes_in_group("enemies").size()
 	if clear == 0:
-		get_tree().change_scene_to_file("res://Scenes/lobby.tscn")
+		get_tree().change_scene_to_file("res://Scenes/stage_3.tscn")
 	if !Global.playerAlive:
 		Global.gameStarted = false
 		get_tree().change_scene_to_file("res://Scenes/lobby.tscn")
--- Scenes/stage_3.gd
+++ Scenes/stage_3.gd
@@ -1,11 +1,22 @@
 extends Node2D
 
+@onready var player_camera = $Player/Camera2D
+@onready var player_camera2 = $Player/stage2_cam
+@onready var player_camera3 = $Player/stage3_cam
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+func _on_something_happened(node_to_destroy):
+	node_to_destroy.queue_free()
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _ready():
+	
+	player_camera3.enabled = true
+	Global.playerWeaponEquipped = true
+	_on_something_happened(player_camera)
+	_on_something_happened(player_camera3)
+func _process(delta):
+	var clear = get_tree().get_nodes_in_group("enemies").size()
+	if clear == 0:
+		get_tree().change_scene_to_file("res://Scenes/stage_3.tscn")
+	if !Global.playerAlive:
+		Global.gameStarted = false
+		get_tree().change_scene_to_file("res://Scenes/lobby.tscn")
